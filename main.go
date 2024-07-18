4c3f02682992b7be63fc89a887bca236c13c57b0
ombima56 <ombimahillary6@gmail.com>
2024-07-18 06:01:31 +0300

    Update: Specify configurable port selection option apart from the default

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"ascii-art-stylize/routes"
 	"log"
 	"net/http"
-
-	"ascii-art-stylize/routes"
+	"os"
+	"strconv"
 )
 
 func main() {
+	port := "8080" // Default port
+	if len(os.Args) > 2 {
+		log.Fatal("Usage: go run main.go [port]")
+		return
+	}
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+		parsedPort, err := strconv.Atoi(port)
+		if err != nil || parsedPort < 1024 || parsedPort > 65535 {
+			log.Fatalf("Invalid port number: %v. It should be between 1024 and 65535.", port)
+			return
+		}
+	}
+
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
 	routes.RoutesSetUp(mux)
-	log.Println("Server Listening on http://localhost:8080")
-	http.ListenAndServe(":8080", mux)
+	log.Printf("Server listening on http://localhost:%s", port)
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
